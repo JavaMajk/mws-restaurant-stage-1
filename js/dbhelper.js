f1ff4516f2543aa73b4f58ddaef0df2bb627db6f
Michal <majkwel@yahoo.com>
2018-05-13 06:05:05 +0200

    changed json file location in dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 7777 // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    return `https://github.com/JavaMajk/mws-restaurant-stage-1/blob/master/data/restaurants.json`;
   }
 
   /**
