0b0f0cf4f41e15b56c56afc22390a7c8ef60d152
Michal <majkwel@yahoo.com>
2018-05-15 14:46:00 +0200

    temporary fix for bluring images with setTimeout()

--- js/main.js
+++ js/main.js
@@ -170,7 +170,12 @@ createRestaurantHTML = (restaurant) => {
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
-
+  setTimeout(() => {
+        if (image.getAttribute('data-src')) {
+          image.setAttribute('src', image.getAttribute('data-src'));
+          image.classList.remove('blur');
+        }
+  }, 1000);
   return li
 }
 
