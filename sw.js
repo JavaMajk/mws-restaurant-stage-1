3d94d80cad0150b1c015e7a18de6e9b6a46e01af
Michal <majkwel@yahoo.com>
2018-05-15 23:36:08 +0200

    trying to fix service worker

--- sw.js
+++ sw.js
@@ -17,8 +17,10 @@ this.addEventListener('install', (event) => {
 });
 
 this.addEventListener('fetch', (event) => {
-  event.respondWith(caches.match(event.request)
-    .then(function (response) {
-      return response || fetch(event.request);
-    }));
+  event.respondWith(
+    caches.match(event.request)
+    .catch(() => {
+      return event.default();
+    })
+  )
 });
